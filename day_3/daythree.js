c349f6e8635c337283e2cde01a43463ecf0f0f34
Hannah Wenger <hamecow@gmail.com>
2021-12-21 21:16:31 -0500

    Create a binary key in hopes of speedier mapping

--- day_3/daythree.js
+++ day_3/daythree.js
@@ -1,7 +1,11 @@
-const diagnosticReport = ["0", "100", "891234"];
+//Numbers 0 - 9 to be converted into a binary key
+const key = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+const binaryKey = key.map((x) => (x >>> 0).toString(2));
 
-let binaryDiagnostics = diagnosticReport.map((x) => (x >>> 0).toString(2));
-console.log(binaryDiagnostics);
+const diagnosticReport = [234, 567, 678, 999];
+const stringDiagnostics = diagnosticReport.map((x) => x.toString());
+console.log(stringDiagnostics);
+console.log(binaryKey);
 //diagnosticReport == puzzle input
 
 //gammaRate == most common bit in the same postion of all numbers in diagnostic report
